39e5de0962920d889f45baf9a6f1f806bf756ca2
Leonardo Martins <leomartins2001@gmail.com>
2022-05-09 12:06:46 -0300

    :sparkles: Add ScoreBoard CSSS

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,8 +1,9 @@
 import PlayerName from "../../components/PlayerName";
+import "./style.css"
 
 export default function ScoreBoard() {
   return /*html*/ `
-    <header class"score-board"">
+    <header class="score-board">
         ${PlayerName("Player 1")}
         ${PlayerName("Player 2")}
     </header>
