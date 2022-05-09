277c4065d195e3b8a84e32a157fa09ee33092d11
Leonardo Martins <leomartins2001@gmail.com>
2022-05-09 11:06:49 -0300

    :lipstick: Create ScoreBoard object

---
+++ src/objects/ScoreBoard/index.js
@@ -0,0 +1,10 @@
+import PlayerName from "../../components/PlayerName";
+
+export default function ScoreBoard() {
+  return /*html*/ `
+    <header class"score-board"">
+        ${PlayerName("Player 1")}
+        ${PlayerName("Player 2")}
+    </header>
+    `;
+}
